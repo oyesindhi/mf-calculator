c70a31720794ba47b64051642b2f8fb86ed6e07b
Shubham Harwani <78555897+oyesindhi@users.noreply.github.com>
2023-02-08 22:15:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,17 +21,14 @@ with tab1:
     months = duration * 12
     
     invested_value = sip_amount*months
-    invested_value_inwords = locale.currency(sip_amount*months,grouping=True)
 
     future_value = sip_amount * ((((1 + monthly_rate)**(months))-1) * (1 + monthly_rate))/monthly_rate
-    future_value_inwords = locale.currency(future_value,grouping=True)
 
     gain = float(future_value) - float(invested_value)
-    gain_inwords = locale.currency(gain,grouping=True)
 
-    st.subheader(f'Amount Invested: {invested_value_inwords}')
-    st.subheader(f'Final Amount: {future_value_inwords}')
-    st.subheader(f'Gain: {gain_inwords}')
+    st.subheader(f'Amount Invested: {invested_value}')
+    st.subheader(f'Final Amount: {future_value}')
+    st.subheader(f'Gain: {gain}')
 
     st.markdown('##')
 
@@ -41,39 +38,33 @@ with tab1:
         months = duration * 12
         
         invested_value = sip_amount*months
-        invested_value_inwords = locale.currency(sip_amount*months,grouping=True)
 
         future_value = sip_amount * ((((1 + monthly_rate)**(months))-1) * (1 + monthly_rate))/monthly_rate
-        future_value_inwords = locale.currency(future_value,grouping=True)
 
-        st.subheader(f'After Inflation: {future_value_inwords}')
+        st.subheader(f'After Inflation: {future_value}')
 
 
 # with tab 2
 with tab2:
     lumpsum_amount = st.number_input('Investment Amount',100,9999999999,1000)
-    lumpsum_amount_inwords = locale.currency(lumpsum_amount,grouping=True)
 
     lumpsum_rate_of_return = st.number_input('Expected Rate of Return (in %)',1.00,100.0,12.0,0.01)
     lumpsum_duration = st.number_input('Duration of Investment (in years)',1,99,10)
 
     cagr = lumpsum_amount * (pow((1 + lumpsum_rate_of_return/100),lumpsum_duration))
-    cagr_inwords = locale.currency(cagr,grouping=True)
     
     lumpsum_gain = float(cagr) - float(lumpsum_amount)
-    lumpsum_gain_inwords = locale.currency(lumpsum_gain,grouping=True)
 
-    st.subheader(f'Amount Invested: {lumpsum_amount_inwords}')
-    st.subheader(f'Final Amount: {cagr_inwords}')
-    st.subheader(f'Gain: {lumpsum_gain_inwords}')
+    st.subheader(f'Amount Invested: {lumpsum_amount}')
+    st.subheader(f'Final Amount: {cagr}')
+    st.subheader(f'Gain: {lumpsum_gain}')
 
     st.markdown('##')
     
     if st.checkbox('Adjust Investment for Inflation ? (Assumed annual inflation rate is 6%)',False) == True:
         lumpsum_present_value = cagr / (pow(1.06,duration))
-        lumpsum_present_value_inwords = locale.currency(lumpsum_present_value,grouping=True)
         
-        st.subheader(f'After Inflation: {lumpsum_present_value_inwords}')
+        st.subheader(f'After Inflation: {lumpsum_present_value}')
 
 
 st.markdown('##')
